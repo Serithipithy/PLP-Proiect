42537d7a3f8874f92f6342d3bc6dfebd87bd69bf
Serithipithy <58854430+Serithipithy@users.noreply.github.com>
2020-12-19 16:43:17 +0200

    Update project.v

--- project.v
+++ project.v
@@ -8,9 +8,17 @@ From Coq Require Import Lia.
 From Coq Require Import Lists.List.
 From Coq Require Import Strings.String.
 Require Import FunInd FMapInterface.
+From Coq Require Import Ascii.
 Import ListNotations.
+Require Import Le Gt Minus Min Bool.
+Require Import Setoid.
+
+Set Implicit Arguments.
 
 (* SINTAXA *)
+Inductive string : Set :=
+  | EmptyString : string
+  | String : ascii -> string -> string.
 
 Definition Env := string -> nat.
 
@@ -83,9 +91,9 @@ Fixpoint beval_fun (b : bexp) (env : Env) : bool :=
                   | false, false => false
                   end
   end.
-Inductive listNat : Type :=
+(*Inductive listNat : Type :=
 | nil
-| cons ( n : string ) ( l : listNat) .
+| cons ( n : string ) ( l : listNat) .*)
 
 Notation "'declare' var" :=(avar var) (at level 40). 
 Notation "A +' B" := (aplus A B) (at level 48).
@@ -100,9 +108,9 @@ Notation "A ==' B" := (bequal A B) (at level 70, no associativity).
 Notation "A >=' B" := (blessthan B A) (at level 53).
 Infix "and'" := band (at level 80).
 
-Notation " x :=: l" := (cons x l) (at level 60, right associativity).
+(*Notation " x :=: l" := (cons x l) (at level 60, right associativity).
 Notation "[ ]" := nil.
-Notation "[ x ; .. ; y ]" := (cons x .. (cons y nil) ..).
+Notation "[ x ; .. ; y ]" := (cons x .. (cons y nil) ..).*)
 
 Inductive Stmt :=
 | assignment : string -> aexp -> Stmt
@@ -117,3 +125,73 @@ Notation "'ifs' cond 'den' { stmt }" := (iffsimpl cond stmt) (at level 93).
 Notation "'ifd' cond 'denn' { stmt1 } 'els' { stmt2 }" := (iff cond stmt1 stmt2) (at level 93).
 Notation "'While' ( B ) { S }" := (while B S) (at level 97).
 Notation "'phor' ( s1 ~ cond ~ s2 ) { stmt }" := (s1 ;; While ( cond ) { stmt ;; s2 }  ) (at level 97).
+Notation "'do' { stmt } 'whilee' ( cond )" := ( stmt ;; While ( cond ) { stmt } ) (at level 97).
+
+Require Import String.
+Open Scope string_scope.
+(* itoa *)
+Class Shows A : Type :=
+  {
+    itoa : A -> string;
+  }.
+Class Shown A : Type :=
+  {
+    atoi : A -> nat
+  }.
+Fixpoint nat_to_string_aux (time n : nat) (acc : string) : string :=
+  let d := match n mod 10 with
+           | 0 => "0" | 1 => "1" | 2 => "2" | 3 => "3" | 4 => "4" | 5 => "5"
+           | 6 => "6" | 7 => "7" | 8 => "8" | _ => "9"
+           end in
+  let acc' := d ++ acc in (*append*)
+  match time with
+    | 0 => acc'
+    | S time' =>
+      match n / 10 with
+        | 0 => acc'
+        | n' => nat_to_string_aux time' n' acc'
+      end
+  end.
+
+Definition nat_to_string (n : nat) : string :=
+  nat_to_string_aux n n "".
+Instance showNat : Shows nat :=
+  {
+    itoa := nat_to_string
+  }.
+Compute (itoa 42).
+
+Definition is_nat (x : ascii) : nat :=
+  match x with
+  | "0" => 0
+  | "1" => 1
+  | "2" => 2
+  | "3" => 3
+  | "4" => 4
+  | "5" => 5
+  | "6" => 6
+  | "7" => 7
+  | "8" => 8
+  | "9" => 9
+  | _ =>   0
+  end%char.
+
+
+Definition first_ch (l:list ascii) : ascii := (* Preia primul elem din lista *)
+    match l with
+      | nil => " "
+      | a :: nill => a
+    end.
+Definition remuve_first_ch (l:list ascii) : list ascii := (* Sterge primul elem din lista *)
+    match l with
+      | nil => []
+      | a :: m => m
+    end.
+Fixpoint list_ascii_of_string (s : string) : list ascii (* De la string la lista ascii *)
+  := match s with
+     | EmptyString => []
+     | String ch s => cons ch (list_ascii_of_string s)
+     end.
+
+
+
